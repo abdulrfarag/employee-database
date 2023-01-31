2943dd35a0a20bad032e07b331380c4bc7690f67
abdulrfarag <ar060290@gmail.com>
2023-01-31 00:19:36 -0600

    added manager to employee

--- db/schema.sql
+++ db/schema.sql
@@ -7,19 +7,18 @@ CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
-CREATE TABLE role(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL Unique,
-  salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id) references department(id) on delete cascade
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
-CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id) references role(id) on delete cascade,
-  FOREIGN KEY (manager_id) references employee(id) on delete set NULL
-  );
+  role_id INTEGER,
+  manager_id INTEGER NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) );
